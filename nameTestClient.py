086e27185c10dad4ffe08f61421491a9ce56569e
jimskon <jamesskon@gmail.com>
2019-10-21 15:05:42 -0400

    Update nameTestClient.py

--- nameTestClient.py
+++ nameTestClient.py
@@ -10,8 +10,10 @@ resultFifoFile = "/tmp/"+fifoname+"_resultFifo"
 #Create Fifos if they don't exist
 if not path.exists(commandFifoFile):
         os.mkfifo(commandFifoFile)
+        os.chmod(commandFifoFile, 0o777)
 if not path.exists(resultFifoFile):
         os.mkfifo(resultFifoFile)
+        os.chmod(resultFifoFile, 0o777)
 
 type=int(input("1=female,2=male,3=last: "))
 name=input("Name:").upper()
